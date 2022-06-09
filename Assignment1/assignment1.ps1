3de233be00764e50f8b47ea0c4719a266d2823ab
akhileshvj <akhilesh75vijay@gmail.com>
2022-06-09 12:21:21 +0530

    first

--- Assignment1/assignment1.ps1
+++ Assignment1/assignment1.ps1
@@ -1,7 +1,7 @@
 #REading user inputs
 [int]$P = Read-host"Please enter the principal amount"
 [int]@R = Read-host"Please enter the rate of interest"
-[int]@T = Read-host"Please enter the number of years
+[int]@T = Read-host"Please enter the number of years"
 
 
 echo "The Simple interest  the values is I"
