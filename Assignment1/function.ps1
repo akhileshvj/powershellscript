2802ea64f77fadb9e061227df0aad4960eba5a6d
akhileshvj <akhilesh75vijay@gmail.com>
2022-06-10 11:02:30 +0530

    first

--- Assignment1/function.ps1
+++ Assignment1/function.ps1
@@ -1,11 +1,12 @@
-function Say-Hello($a,$b) {
+function Say-Hello($a, $b) {
     foreach ($i in $a){
         Write-Output "Hi all, this is $i"
     }
-    Write-Output " $b"
+    Write-Output "$b"
 }
 
 $names_array = @("akku","makku","chikku","titu")
+$geetings = "Hellow, How r u?"
+
 Say-Hello $names_array $geetings
 
-$geetings = "Hellow, How r u?"
