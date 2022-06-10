96e3b8c7a887294cb7fcab21b1d2cbfd2de1a503
akhileshvj <akhilesh75vijay@gmail.com>
2022-06-10 11:09:25 +0530

    first

--- Assignment1/function.ps1
+++ Assignment1/function.ps1
@@ -10,3 +10,14 @@ $geetings = "Hellow, How r u?"
 
 Say-Hello $names_array $geetings
 
+
+function My-Intro {
+    param(
+        [string]$Name,
+        [string]$Profession,
+        [int]$age
+         )
+    Write-Output "My name is $Name and job is $Profession and age is $age"
+}
+
+My-Intro -name "akhilesh" -profession "associate" -age "26"
